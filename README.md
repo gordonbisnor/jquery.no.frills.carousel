1b3e556e95da4dc503392e7da615b59124b4f0ec
Gordon B. Isnor <gordonbisnor@gmail.com>
2013-04-24 16:35:50 -0300

    First version of the plugin
    First version of the plugin
    
    Signed-off-by: Gordon B. Isnor <gordonbisnor@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 jquery.no.frills.carousel
 =========================
 
-jquery.no.frills.carousel
+The world needs more jQuery carousels, so I'm doing what I can.
+
+I was using Tiny Carousel, which was serving my needs just fine, but it mysteriously broke on me so I created this. It that works for you it's recommended over this. It has options, this has none.
+
+Install
+-------
+
+    <script src="jquery.no.frills.carousel.min.js"></script>
+
+Use
+---
+
+    <script type="text/javascript">
+        $(document).ready(function(){ 
+          $('.carousel').tinycarousel(); 
+        });
+    </script> 
+
+
+HTML Structure 
+--------------
+
+    <div class="carousel">
+      <a class="prev" href="#">Previous</a>
+      <div class="viewport">
+        <ul class="overview">
+          <li><img src="my_image.png"/></li>
+        </ul>
+      </div>
+      <a class="next" href="#">Next</a>
+    </div>
---
+++ jquery.no.frills.carousel.js
@@ -0,0 +1,53 @@
+(function($) {
+
+  $.fn.NoFrillsCarousel = function() {
+
+    return $(this).each(function () {
+
+      var $this = $(this);
+            
+      var overview = $this.find('ul.overview');
+      
+      /* number of items being displayed */
+      var count = overview.find('li').length;
+
+      /* width of items including margin and padding */
+      var width = overview.find('li').first().outerWidth();
+        
+      var viewport = $this.find('div.viewport');
+
+      var possibleWidth = viewport.outerWidth();
+      
+      /* number of items that are visible at one time */
+      var itemsToDisplayCount = possibleWidth / width;
+
+      viewport.css('overflow', 'hidden');
+      
+      var position = 0;
+
+      /* backwards */
+      $this.find('a.prev').on('click', function(){
+        if(position > 0) {
+          var new_position = "+=" + width;
+          overview.animate({left: new_position}, 100);
+          position--;
+        };
+        return false;
+      }); /* end backwards */
+
+      /* forwards */
+      $this.find('a.next').on('click', function(){
+        if(position - (count - itemsToDisplayCount) < 0) {
+          var overview = $this.find('ul.overview');
+          var new_position = "-=" + width;
+          overview.animate({left: new_position}, 100);
+          position++;
+        };
+        return false;
+      }); /* end forwards */
+
+    }); 
+
+  };
+
+})( jQuery );
